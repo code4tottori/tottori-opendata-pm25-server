ba4a321c6b6b03d81c51a1b4b59b398e819c725d
Masayuki Higashino <msyk@hgsn.info>
2017-01-24 04:16:06 +0900

    fix

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -13,25 +13,38 @@ class RecordsController < ApplicationController
     end
     # DBを調べる
     @record = Record.find_by(date:@date)
-    # 当該日の最終データまで完全に保存されている場合はDBから返却
-    if @record && @record.updated_at.utc >= @record.date.tomorrow.to_time.utc
-      render json:@record.data and return
-    end
-    # 鳥取県のウェブサイトには6日前までしかデータを公開していない
-    # その日よりも前のデータは存在しないので不完全なデータであってもDBから返却
-    if @record && @date < Time.now.ago(6.days).in_time_zone('Asia/Tokyo').to_date
-      render json:@record.data and return
-    end
-    # データ取得
-    ActiveSupport::JSON::Encoding.use_standard_json_time_format = true
-    @data = Tottori::OpenData::PM25::API.get(@date.in_time_zone('Asia/Tokyo')).to_json
+    # DBに存在する
     if @record
+      # 当該日の最終データまで完全に保存されている場合はDBから返却
+      if @record.updated_at.utc >= @record.date.tomorrow.to_time.utc
+        render json:@record.data and return
+      end
+      # 鳥取県のウェブサイトには6日前までしかデータを公開していない
+      # その日よりも前のデータは存在しないので不完全なデータであってもDBから返却
+      if @date < Time.now.ago(6.days).in_time_zone('Asia/Tokyo').to_date
+        render json:@record.data and return
+      end
+      # データ取得
+      ActiveSupport::JSON::Encoding.use_standard_json_time_format = true
+      @data = Tottori::OpenData::PM25::API.get(@date.in_time_zone('Asia/Tokyo')).to_json
+      # DB更新
       if @record.update(data:@data)
         render json:@record.data, status: :ok and return
       else
         render json:@record.errors, status: :unprocessable_entity and return
       end
-    else
+    end
+    # DBに存在しない
+    if !@record
+      # 鳥取県のウェブサイトには6日前までしかデータを公開していない
+      # その日よりも前のデータは存在しないので404
+      if @date < Time.now.ago(6.days).in_time_zone('Asia/Tokyo').to_date
+        render json:{}, status: :not_found and return
+      end
+      # データ取得
+      ActiveSupport::JSON::Encoding.use_standard_json_time_format = true
+      @data = Tottori::OpenData::PM25::API.get(@date.in_time_zone('Asia/Tokyo')).to_json
+      # DB追加
       @record = Record.new(date:@date, data:@data)
       if @record.save
         render json:@record.data, status: :created and return
