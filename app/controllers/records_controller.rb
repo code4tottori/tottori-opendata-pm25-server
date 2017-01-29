f45bbf09eca8e3e5b8434b6d0b011ecf8badb5f4
Masayuki Higashino <msyk@hgsn.info>
2017-01-29 15:13:36 +0900

    refactor

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -49,50 +49,15 @@ class RecordsController < ApplicationController
   # GET /records/20170124.json
   def show
     @date = Date.parse(params[:date])
-    # 未来のデータは存在しないので404
-    if @date > Time.now.in_time_zone('Asia/Tokyo').to_date
-      render json:[], status: :not_found and return
-    end
-    # DBを調べる
-    @record = Record.find_by(date:@date)
-    # DBに存在する
+    @record = Record.fetch(@date)
     if @record
-      # 当該日の最終データまで完全に保存されている場合はDBから返却
-      if @record.updated_at.utc >= @record.date.tomorrow.to_time.utc
-        render json:@record.data and return
-      end
-      # 鳥取県のウェブサイトには6日前までしかデータを公開していない
-      # その日よりも前のデータは存在しないので不完全なデータであってもDBから返却
-      if @date < Time.now.ago(6.days).in_time_zone('Asia/Tokyo').to_date
-        render json:@record.data and return
-      end
-      # データ取得
-      ActiveSupport::JSON::Encoding.use_standard_json_time_format = true
-      @data = Tottori::OpenData::PM25::API.get(@date.in_time_zone('Asia/Tokyo')).to_json
-      # DB更新
-      if @record.update(data:@data)
+      if @record.errors.empty?
         render json:@record.data, status: :ok and return
       else
         render json:@record.errors, status: :unprocessable_entity and return
       end
-    end
-    # DBに存在しない
-    if !@record
-      # 鳥取県のウェブサイトには6日前までしかデータを公開していない
-      # その日よりも前のデータは存在しないので404
-      if @date < Time.now.ago(6.days).in_time_zone('Asia/Tokyo').to_date
-        render json:[], status: :not_found and return
-      end
-      # データ取得
-      ActiveSupport::JSON::Encoding.use_standard_json_time_format = true
-      @data = Tottori::OpenData::PM25::API.get(@date.in_time_zone('Asia/Tokyo')).to_json
-      # DB追加
-      @record = Record.new(date:@date, data:@data)
-      if @record.save
-        render json:@record.data, status: :created and return
-      else
-        render json:@record.errors, status: :unprocessable_entity and return
-      end
+    else
+      render json:[], status: :not_found and return
     end
   rescue ArgumentError => e
     render json:e.to_json, status: :unprocessable_entity and return
--- app/models/record.rb
+++ app/models/record.rb
@@ -4,4 +4,46 @@ class Record < ApplicationRecord
     Record.order(:updated_at).select(:updated_at).last&.updated_at
   end
 
+  def self.fetch(date = Time.now.in_time_zone('Asia/Tokyo').to_date)
+    # 未来のデータは存在しない
+    if date > Time.now.in_time_zone('Asia/Tokyo').to_date
+      return nil
+    end
+    # DBを調べる
+    record = Record.find_by(date:date)
+    # DBに存在する
+    if record
+      # 当該日の最終データまで完全に保存されている場合はDBから返却
+      if record.updated_at.utc >= record.date.tomorrow.to_time.utc
+        return record
+      end
+      # 鳥取県のウェブサイトには6日前までしかデータを公開していない
+      # その日よりも前のデータは存在しないので不完全なデータであってもDBから返却
+      if date < Time.now.ago(6.days).in_time_zone('Asia/Tokyo').to_date
+        return record
+      end
+      # データ取得
+      ActiveSupport::JSON::Encoding.use_standard_json_time_format = true
+      data = Tottori::OpenData::PM25::API.get(date.in_time_zone('Asia/Tokyo')).to_json
+      # DB更新
+      record.update(data:data)
+      return record
+    end
+    # DBに存在しない
+    if !record
+      # 鳥取県のウェブサイトには6日前までしかデータを公開していない
+      # その日よりも前のデータは存在しない
+      if date < Time.now.ago(6.days).in_time_zone('Asia/Tokyo').to_date
+        return nil
+      end
+      # データ取得
+      ActiveSupport::JSON::Encoding.use_standard_json_time_format = true
+      data = Tottori::OpenData::PM25::API.get(date.in_time_zone('Asia/Tokyo')).to_json
+      # DB追加
+      record = Record.new(date:date, data:data)
+      record.save
+      return record
+    end
+  end
+
 end
